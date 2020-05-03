b003576a10aa992cd033a0959fbc4814176f6219
Jude Tomas <eduj1samot@gmail.com>
2020-05-02 21:30:04 -0400

    Iterated over questions to count for traits

--- mysite/survey/views.py
+++ mysite/survey/views.py
@@ -12,10 +12,29 @@ def index(request):
     context = {'latest_question_list': latest_question_list}
     return render(request, 'survey/index.html', context)
 def process(request):
-    user_input = request.POST['question1']
-    user_answer = Answer.objects.get(pk=request.POST['answer'])
-    user_answer_type = user_answer.answer_type
-    print (user_input)
-    print (user_answer)
-    print (user_answer_type)
+    ei = 0
+    ns = 0
+    tf = 0
+    pj = 0
+    for i in range(len(Question.objects.all())):
+        user_input = request.POST.get('question' + str(i+1), False)
+        user_answer = Answer.objects.get(pk=user_input)
+        user_answer_type = user_answer.answer_type
+        if user_answer_type == 'E':
+            ei += 1
+        elif user_answer_type == 'I':
+            ei -= 1
+        elif user_answer_type == 'N':
+            ns += 1
+        elif user_answer_type == 'S':
+            ns -= 1
+        elif user_answer_type == 'T':
+            tf += 1
+        elif user_answer_type == 'F':
+            tf -= 1
+        elif user_answer_type == 'P':
+            pj += 1
+        elif user_answer_type == 'J':
+            pj -= 1
+        print(user_answer)
     return "Hi!"
